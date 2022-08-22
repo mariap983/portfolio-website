9eff23a9911b797316b33437a445c36064c8830b
mariap983 <maria.petrovaa983@gmail.com>
2022-08-22 15:03:55 +0300

    .

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -26,8 +26,8 @@ const Portfolio = () => {
                     portfolio.map((port, idx) => {
                         return (
                             <div className="image-box" key={idx}>
-                                <img 
-                                src={port.cover}
+                               <img 
+                                src={process.env.PUBLIC_URL + port.cover}
                                 className="portfolio-image"
                                 alt="portfolio" 
                                 />
